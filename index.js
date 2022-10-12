402cf5407dadb6d05e688dd771bc35d43168c388
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-10-12 19:21:51 +0300

    Bugfix moveSlides func

--- index.js
+++ index.js
@@ -2,56 +2,104 @@ const itemEls = document.querySelectorAll('.slide-item');
 const nextButtonEl = document.querySelector('.next');
 const prevButtonEl = document.querySelector('.prev');
 const slideListEl = document.querySelector('.slide-list');
-
-const itemArr = [...itemEls];
-
-let num = 1;
-const total = itemArr.length;
+const slideNumberEls = document.querySelectorAll('.slide-number');
+const dotContainerEl = document.querySelector('.dots-container');
 
 nextButtonEl.addEventListener('click', onNextButtonClick);
 prevButtonEl.addEventListener('click', onPrevButtonClick);
 slideListEl.addEventListener('click', onImageClick);
 
+const itemArr = [...itemEls];
+let index = 0;
+
+// adds slide numbers
+let num = 1;
+const total = itemArr.length;
+
+slideNumberEls[index].textContent = `${index + 1}/${total}`;
+
+// adds dots
+itemArr.map(el => {
+    const dot = document.createElement("div");
+    dot.classList.add('dot');
+    dotContainerEl.append(dot);
+})
+
+const dotEls = document.querySelectorAll('.dot');
+dotEls[index].style.backgroundColor = "black";
+
 function onNextButtonClick() {
-    num += 1;
-    for (let index = 0; index < itemArr.length; index += 1) {
+    
+    moveSlides(1);
+
+    
+    // dotEls[num].style.backgroundColor = "black";
         
-        prevButtonEl.classList.remove('disabled');
+    // num += 1;
+    // slideNumberEls[num - 1].textContent = `${num}/${total}`;
+    
+    // for (let index = 0; index < itemArr.length; index += 1) {
+    //     dotEls[index].style.backgroundColor = "blue";
+    //     prevButtonEl.classList.remove('disabled');
         
-        const current = itemArr[index];
-        const next = itemArr[index + 1];        
+    //     const current = itemArr[index];
+    //     const next = itemArr[index + 1];        
         
-        if (index === itemArr.length - 2) {
-            nextButtonEl.classList.add('disabled');
-        }
+    //     if (index === itemArr.length - 2) {
+    //         nextButtonEl.classList.add('disabled');
+    //     }
         
-        if (itemArr[index].className.includes('is-active') && index !== itemArr.length - 1) {
-            next.classList.add('is-active');
-            current.classList.remove('is-active');
-            return;
-        }    
-    }
+    //     if (itemArr[index].className.includes('is-active') && index !== itemArr.length - 1) {
+    //         next.classList.add('is-active');
+    //         current.classList.remove('is-active');
+    //         return;
+    //     }    
+    // }
 }
 
 
-function onPrevButtonClick() {
-    for (let index = itemArr.length - 1; index > 0; index -= 1) {
+function moveSlides(param) {
+    const current = itemArr[index];
+    let next = itemArr[index + param];
 
-        nextButtonEl.classList.remove('disabled');
-        
-        const current = itemArr[index];
-        const prev = itemArr[index - 1];
+    index = index + param;
 
-        if (index === 1) {
-            prevButtonEl.classList.add('disabled');
-        }
-
-        if (itemArr[index].className.includes('is-active')) {
-            prev.classList.add('is-active');
-            current.classList.remove('is-active');
-            return;
-        }    
+    if (index === total) {
+        index = 0;
+        next = itemArr[index]
+    } else if (index < 0) {
+        index = total - 1;
+        next = itemArr[index]
     }
+   
+    next.classList.add('is-active');
+    current.classList.remove('is-active');
+} 
+
+
+function onPrevButtonClick() {
+
+moveSlides(-1);
+
+    // for (let index = itemArr.length - 1; index > 0; index -= 1) {
+    //     dotEls[index].style.backgroundColor = "blue";
+    //     dotEls[index - 1].style.backgroundColor = "black";
+
+    //     nextButtonEl.classList.remove('disabled');
+        
+    //     const current = itemArr[index];
+    //     const prev = itemArr[index - 1];
+
+    //     if (index === 1) {
+    //         prevButtonEl.classList.add('disabled');
+    //     }
+
+    //     if (itemArr[index].className.includes('is-active')) {
+    //         prev.classList.add('is-active');
+    //         current.classList.remove('is-active');
+    //         return;
+    //     }    
+    // }
 }
 
 function onImageClick(e) {
